c7ff8ee95a89d6cc291e9bb8c8b56cfbf6c52de6
Abdiraimova Meerim <113308029+obllivon@users.noreply.github.com>
2022-12-14 12:21:47 +0600

    Add files via upload

---
+++ React_dz2/src/MyInfo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function MyInfo() {
+  return (
+    <div>
+      <h1>Meerim</h1>
+      <p>Меня зовут Мээрим. Я учусь в турецком женском лицее.</p>
+      <ol>
+        <li>Швейцария</li>
+        <li>Япония</li>
+        <li>Канада</li>
+      </ol>
+    </div>
+  )
+}
